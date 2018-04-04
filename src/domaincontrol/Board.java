a9d5b982e850ef5285a0f788cbb40d099e456727
pol.valls.ortiz <pol.valls.ortiz@est.fib.upc.edu>
2018-04-04 19:22:03 +0200

    Topographical error!

--- src/domaincontrol/Board.java
+++ src/domaincontrol/Board.java
@@ -6,7 +6,7 @@ import java.util.Vector;
 public abstract class Board {
     Map<Integer, Integer> cellPositions;
     Vector<Cell> vectorCell;
-    Map<Integer, Vector<Integer>> adjacecyMatrix;
+    Map<Integer, Vector<Integer>> adjacencyMatrix;
 
     public void createBoard(Vector<Vector<String>> matrix) {
 
