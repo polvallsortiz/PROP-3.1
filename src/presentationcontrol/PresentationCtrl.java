0da14a18f12c6fa9bdb4bc5994d5da517b5d614a
pol.valls.ortiz <pol.valls.ortiz@est.fib.upc.edu>
2018-04-04 13:16:27 +0200

    Gitignore update

--- src/presentationcontrol/PresentationCtrl.java
+++ src/presentationcontrol/PresentationCtrl.java
@@ -1,11 +1,14 @@
 package presentationcontrol;
 
+import domaincontrol.DomainCtrl;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Vector;
 
 public class PresentationCtrl {
+    static String username;
     static Vector<Vector<String>>  hidato= new Vector<Vector<String>>();
     static Character celltype;
     static String adjacencytype;
@@ -30,8 +33,10 @@ public class PresentationCtrl {
     }
 
     public static int newGame(String player)    {
-        System.out.print("Cell Type? [Q,H,T],Adjacency Type? [C,CA],Lines Number,Column Number\n THEN ALL THE LINES OF HIDATO \n");
         Scanner scan = new Scanner(System.in);
+        System.out.print("Enter username: ");
+        username = scan.nextLine();
+        System.out.print("Cell Type? [Q,H,T],Adjacency Type? [C,CA],Lines Number,Column Number\n THEN ALL THE LINES OF HIDATO \n");
         String input = "";
         while(input.length() == 0) input = scan.nextLine();
         extract_data(input);
@@ -41,14 +46,17 @@ public class PresentationCtrl {
             hidato.add(extract_line(input));
         }
         printHidato();
+        DomainCtrl dc = new DomainCtrl();
+        dc.createGame(username);
         return lines;
 
     }
 
     public static void printHidato() {
-        System.out.print("\n" + celltype + "\n");
-        System.out.print(adjacencytype + "\n");
-        System.out.print(lines + " "  + columns + "\n");
+        System.out.print("Hola " + username + " benvingut al carregador d'Hidatos! \n");
+        System.out.print("Has definit el tipus de cel·la com " + celltype + "\n");
+        System.out.print("Amb adjacència " + adjacencytype + "\n");
+        System.out.print("I finalment amb " + lines + " liníes i "  + columns + " columnes\n");
         System.out.print("\\\\\\\\ HIDATO COMING ///////\n");
         for(int i = 0; i < hidato.size(); ++i) {
             Vector<String> aux = new Vector<>();
