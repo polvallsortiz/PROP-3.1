b656bc104356bd6cb70707114dc5266a766d58e4
Joan Sanchez PC <jsanchezgarcia13@gmail.com>
2018-04-08 12:45:18 +0200

    Updated tests for previous conditions

--- tests/domaincontrol/BoardTest.java
+++ tests/domaincontrol/BoardTest.java
@@ -26,7 +26,22 @@ class BoardTest {
         adjacencyMatrix.put(2, new ArrayList<Integer>(Arrays.asList(1,3)));
         adjacencyMatrix.put(3, new ArrayList<Integer>(Arrays.asList(0,2)));
         board.setAdjacencyMatrix(adjacencyMatrix);
-        adjacencyMatrix = board.getAdjacencyMatrix();
         assertTrue(board.solveHidato());
+    }
+
+    @Test
+    void TestingPreviousConditionsGraph2(){
+        Map<Integer, ArrayList<Integer>> adjacencyMatrix = new HashMap<>();
+        adjacencyMatrix.put(0, new ArrayList<Integer>(Arrays.asList(2)));
+        adjacencyMatrix.put(1, new ArrayList<Integer>(Arrays.asList(2,3)));
+        adjacencyMatrix.put(2, new ArrayList<Integer>(Arrays.asList(0, 1, 5)));
+        adjacencyMatrix.put(3, new ArrayList<Integer>(Arrays.asList(1)));
+        adjacencyMatrix.put(4, new ArrayList<Integer>(Arrays.asList(5)));
+        adjacencyMatrix.put(5, new ArrayList<Integer>(Arrays.asList(2, 4, 6)));
+        adjacencyMatrix.put(6, new ArrayList<Integer>(Arrays.asList(5, 8)));
+        adjacencyMatrix.put(7, new ArrayList<Integer>(Arrays.asList(8)));
+        adjacencyMatrix.put(8, new ArrayList<Integer>(Arrays.asList(6, 7)));
+        board.setAdjacencyMatrix(adjacencyMatrix);
+        assertFalse(board.solveHidato());
     }
 }
