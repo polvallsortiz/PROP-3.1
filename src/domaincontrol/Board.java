5dbf760bb27d420ebe41426689ab73b39db562dc
Joan Sanchez PC <jsanchezgarcia13@gmail.com>
2018-04-08 12:40:39 +0200

    Previous conditions working

--- src/domaincontrol/Board.java
+++ src/domaincontrol/Board.java
@@ -1,16 +1,46 @@
 package domaincontrol;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Stack;
-import java.util.Vector;
+import java.util.*;
 
 public class Board {
-    Map<Integer, Integer> cellPositions;
-    Vector<Cell> vectorCell;
-    Map<Integer, Vector<Integer> > adjacecieMatrix;
+    private Map<Integer, Integer> cellPositions;
+    private Vector<Cell> vectorCell;
+    private Map<Integer, ArrayList<Integer>> adjacencyMatrix;
+
+    public void setCellPositions(Map<Integer, Integer> cellPositionsExterior){
+        cellPositions = cellPositionsExterior;
+    }
+
+    public void setVectorCell(Vector<Cell> vectorCellExterior){
+        vectorCell = vectorCellExterior;
+    }
+
+    public void setAdjacencyMatrix(Map<Integer, ArrayList<Integer>> adjacencyMatrixExterior){
+        adjacencyMatrix = adjacencyMatrixExterior;
+    }
+    public Map<Integer, ArrayList<Integer>> getAdjacencyMatrix(){
+        return adjacencyMatrix;
+    }
 
     public boolean solveHidato(){
+        if(previousConditions())return true;
+        else return false;
+    }
+
+    private boolean previousConditions() {  //Check some of hamiltonian graphs conditions to know if there is a possible path
+        boolean first = false;
+        int how_many_1 = 0;
+        Iterator<Map.Entry<Integer, ArrayList<Integer>>> it = adjacencyMatrix.entrySet().iterator();
+        while (it.hasNext() && (how_many_1 < 3 || ((how_many_1 == 2) && (first == true)))){
+            Map.Entry<Integer, ArrayList<Integer>> pair = it.next();
+            if (pair.getKey().equals(1)) {first = true;}
+            if (pair.getValue().size()== 1){++how_many_1;}
+        }
+        if (how_many_1 < 3 || ((how_many_1 == 2) && (first == true))) return true;
+        else return false;
+    }
+
+    private boolean recursiveSolver() { //Solves the Hidato recursively
         Boolean[] already_visited = new Boolean[vectorCell.size()];
         Arrays.fill(already_visited, Boolean.FALSE);
         Stack<Integer> stack = null;
@@ -19,7 +49,7 @@ public class Board {
             int next_move = stack.pop();
             if (!already_visited[next_move]){
                 already_visited[next_move] = true;
-                Vector<Integer> edges = adjacecieMatrix.get(next_move);
+                ArrayList<Integer> edges = adjacencyMatrix.get(next_move);
                 for (int iterator = 0; iterator < edges.size(); ++iterator ){
                     stack.push(edges.get(iterator));
                 }
--- tests/domaincontrol/BoardTest.java
+++ tests/domaincontrol/BoardTest.java
@@ -1,12 +1,32 @@
 package domaincontrol;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BoardTest {
 
+class BoardTest {
+    Board board;
     @BeforeEach
     void setUp() {
+         board = new Board();
+    }
+
+    @Test
+    void TestingPreviousConditionsGraph1(){
+        Map<Integer, ArrayList<Integer>> adjacencyMatrix = new HashMap<>();
+        adjacencyMatrix.put(0, new ArrayList<Integer>(Arrays.asList(1,3)));
+        adjacencyMatrix.put(1, new ArrayList<Integer>(Arrays.asList(0,2)));
+        adjacencyMatrix.put(2, new ArrayList<Integer>(Arrays.asList(1,3)));
+        adjacencyMatrix.put(3, new ArrayList<Integer>(Arrays.asList(0,2)));
+        board.setAdjacencyMatrix(adjacencyMatrix);
+        adjacencyMatrix = board.getAdjacencyMatrix();
+        assertTrue(board.solveHidato());
     }
 }
