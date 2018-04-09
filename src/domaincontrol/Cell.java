bcaa5f19cec440fe0e731bd4008669a30d011dfe
Joan <jsanchezgarcia13@gmail.com>
2018-04-09 15:46:52 +0200

    Cell class defined

--- src/domaincontrol/Cell.java
+++ src/domaincontrol/Cell.java
@@ -1,4 +1,37 @@
 package domaincontrol;
 
 public class Cell {
+    private int id;
+    private boolean accessible;
+    private int number;
+
+    public Cell(int id, boolean accessible, int number){
+        this.id = id;
+        this.accessible = accessible;
+        this.number = number;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public boolean getAccessible(){
+        return accessible;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setAccessible(boolean accessible){
+        this.accessible = accessible;
+    }
+
+    public void setNumber(int number){
+        this.number = number;
+    }
 }
