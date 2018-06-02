1f1b718fec2f5b5e8d8df3f38560cd62aa0f11eb
polvallsortiz <pol.valls.ortiz@est.fib.upc.edu>
2018-06-02 13:07:02 +0200

    User experience improved 2000%

--- src/main/java/presentationcontrol/EditHidatoField.java
+++ src/main/java/presentationcontrol/EditHidatoField.java
@@ -1,5 +1,6 @@
 package presentationcontrol;
 
+import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,6 +11,7 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.util.Vector;
 
@@ -17,6 +19,12 @@ public class EditHidatoField {
     private PresentationCtrl pc;
     private TextField tf;
     private Integer i;
+
+    //PROPOSE STRUCTURES
+    private Integer propose; //0 IF NUMBER     1 IF HOLE     2 IF INACCESSIBLE
+    JFXRadioButton numberradio;
+    JFXRadioButton holeradio;
+    JFXRadioButton inaccessibleradio;
 
     Stage sta;
 
@@ -28,36 +36,86 @@ public class EditHidatoField {
         this.i = i;
         this.pc = pc;
         if(pc.getHidato().get(i/pc.getColumns()).get(i%pc.getColumns()).equals("?")) {
-            if (flag != 2) {
-                sta = new Stage();
-                Parent root = FXMLLoader.load(getClass().getResource("/forms/EditHidatoField.fxml"));
-                sta.setTitle("Editar Cel·la - Hidato Game");
-                sta.setScene(new Scene(root, 500, 300));
-                sta.setResizable(false);
-                sta.show();
-                tf = (JFXTextField) sta.getScene().lookup("#textfield");
-                Button bt = (Button) sta.getScene().lookup("#acceptbutton");
-                bt.setOnMouseClicked(e -> {
-                    sta.close();
-                    try {
-                        if (flag == 0) accepted();
-                        else play();
-                    } catch (IOException e1) {
-                        e1.printStackTrace();
-                    }
-                });
+            switch (flag) {
+                case 0: //PROPOSE
+                    sta = new Stage();
+                    Parent root0 = FXMLLoader.load(getClass().getResource("/forms/EditHidatoFieldProposar.fxml"));
+                    sta.setTitle("Editar Cel·la - Hidato Game - Proposar Hidato");
+                    sta.setScene(new Scene(root0,500,300));
+                    sta.setResizable(false);
+                    sta.show();
+                    tf = (JFXTextField) sta.getScene().lookup("#textfield");
+                    Button bt0 = (Button) sta.getScene().lookup("#acceptbutton");
+                    numberradio = (JFXRadioButton) sta.getScene().lookup("#numberradio");
+                    holeradio = (JFXRadioButton) sta.getScene().lookup("#holeradio");
+                    inaccessibleradio = (JFXRadioButton) sta.getScene().lookup("#inaccessibleradio");
+                    bt0.setOnMouseClicked(e->{
+                        try {
+                            accepted();
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        }
+                    });
+                    this.propose = 0;
+                    numberradio.setSelected(true);
+                    numberradio.setOnMouseClicked(e->numberclicked());
+                    holeradio.setOnMouseClicked(e->holeclicked());
+                    inaccessibleradio.setOnMouseClicked(e->inaccessibleclicked());
+                    break;
+
+                case 1: //PLAY
+                    sta = new Stage();
+                    Parent root = FXMLLoader.load(getClass().getResource("/forms/EditHidatoField.fxml"));
+                    sta.setTitle("Editar Cel·la - Hidato Game - Partida en Curs");
+                    sta.setScene(new Scene(root,500,300));
+                    sta.setResizable(false);
+                    sta.show();
+                    tf = (JFXTextField) sta.getScene().lookup("#textfield");
+                    Button bt = (Button) sta.getScene().lookup("#acceptbutton");
+                    bt.setOnMouseClicked(e->{
+                        try {
+                            play();
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        }
+                    });
+                    break;
+
+                case 2: //LEFT CLICK
+                    playnext();
+                    break;
             }
-            else playnext();
         }
     }
 
     private void accepted() throws IOException {
         Vector<Vector<String>> temp = pc.getHidato();
         Vector<String> temp2 = temp.get(i/pc.getColumns());
-        temp2.set(i%pc.getColumns(),tf.getText());
+        switch (propose) {
+            case 0: //NUMBER
+                if(checkrange()) {
+                    sta.close();
+                    temp2.set(i%pc.getColumns(),tf.getText());
+                }
+                else {
+                    tf.setText("");
+                    tf.setPromptText("VALOR FORA DE RANG!");
+                    return;
+                }
+                break;
+
+            case 1: //HOLE
+                sta.close();
+                temp2.set(i%pc.getColumns(),"#");
+                break;
+
+            case 2: //INACCESSIBLE
+                sta.close();
+                temp2.set(i%pc.getColumns(),"*");
+                break;
+        }
         temp.set(i/pc.getColumns(),temp2);
         pc.setHidato(temp);
-        sta.close();
         PrinterHidatoProposar php = new PrinterHidatoProposar(pc);
     }
 
@@ -101,8 +159,16 @@ public class EditHidatoField {
 
     //RIGHT CLICK
     private void play() throws IOException {
-        //pc.getHidato().get(i/pc.getColumns()).set(i%pc.getColumns(),tf.getText());
-        Character result = pc.nextMovement(i,tf.getText());
+        Character result;
+        if(checkrange()) {
+            sta.close();
+            result = pc.nextMovement(i, tf.getText());
+        }
+        else {
+            tf.setText("");
+            tf.setPromptText("VALOR FORA DE RANG!");
+            return;
+        }
         switch (result) {
             case 'C':
                 //CRIDA A COMPLETED
@@ -137,4 +203,36 @@ public class EditHidatoField {
                 break;
         }
     }
+
+    private void numberclicked() {
+        numberradio.setSelected(true);
+        holeradio.setSelected(false);
+        inaccessibleradio.setSelected(false);
+        tf.setDisable(false);
+        propose = 0;
+    }
+
+    private void holeclicked() {
+        holeradio.setSelected(true);
+        numberradio.setSelected(false);
+        inaccessibleradio.setSelected(false);
+        tf.setDisable(true);
+        propose = 1;
+    }
+
+    private void inaccessibleclicked() {
+        inaccessibleradio.setSelected(true);
+        numberradio.setSelected(false);
+        holeradio.setSelected(false);
+        tf.setDisable(true);
+        propose = 2;
+    }
+
+    private boolean checkrange() {
+        int actual = Integer.parseInt(tf.getText());
+        int size = pc.getRows() * pc.getColumns();
+        if(actual < size && actual > 0) return true;
+        else return false;
+    }
+
 }
